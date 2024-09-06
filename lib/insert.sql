67e6d510ea497d77fc50449d72e2dd1396fd9fe3
Wangui <elswangui@gmail.com>
2024-09-06 18:51:07 +0300

    sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Mr.Chocolate', 3, 'M','Chocolate','Quiet','YES');
+
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Rowdy', 12, 'M','White','Happy','YES');
+
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Tabitha', 1, 'F','Brown','Free','NO');
+
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Sergeant Brown', 3, 'M','Brown','Social','NO');
+
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Melissa', 7, 'F','Blue','Calm','YES');
+
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Grinch', 10, 'M','Green','Angry','NO');
+
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Wendy', 5, 'F','Lilac','Loving','YES');
+
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES (NULL,0,'M','Yellow','Cuddly','NO');
